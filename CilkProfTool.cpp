5f4664df51259382fa46ac782a9768b7552cacd3
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2016-05-09 00:16:13 +0200

    template for CilkProfTool

--- CilkProfTool.cpp
+++ CilkProfTool.cpp
@@ -1,15 +1,12 @@
 /*
  * CilkProfTool.h
- *
- *  Created on: Dec 12, 2015
+ 
+ *  Created on: May 9, 2016
  *      Author: knapp
  */
 
-#ifndef CILKPROFTOOL_H_
-#define CILKPROFTOOL_H_
-
-#include "Tool.h"
-
+#include <vector>
+#include "CilkProfTool.h"
 #include <iostream>
 #include <array>
 #include <set>
@@ -23,33 +20,42 @@
 #include "ShadowLock.h"
 #include "DataModel.h"
 #include "DBDataModel.h"
-#include "CilkProf.h"
 #define THREADS 100
 
-typedef std::map<unsigned int, const char*> ThreadFunctionMap;
 
-class CilkProfTool : public Tool {
-public:
+CilkProfTool::CilkProfTool() {
 
-	CilkProfTool();
-	void join(const Event* e);
-	void call(const Event* e);
-	void returnOfCalled(const Event* e);
-	void ~CilkProfTool();
+//
 	
-private:
+}	
 
-	// Provides information to the CilkProf algorithm
-	CilkProf cilkprof;
+CilkProfTool::~CilkProfTool() {
 
-	// keeps track of current function signature for each thread
-	ThreadFunctionMap threadFunctionMap;
+// 
 
-	// prevent generated functions --------------------------------------------
-	CilkProfTool(const CilkProfTool&);
-	CilkProfTool& operator=(const CilkProfTool&);
-};
+}
+
+void CilkProfTool::join(const Event* e) {
+
+//
+
+}
+
+void CilkProfTool::call(const Event* e) {
+
+//
+
+}
+
+void CilkProfTool::returnOfCalled(const Event* e){
+
+// 
+
+}
 
 
 
-#endif /* CILKPROFTOOL_H_ */
+
+
+
+
